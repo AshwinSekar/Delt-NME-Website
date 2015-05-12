3759737dd313f6be41b74dc293423b0fb0205311
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-12 18:49:55 -0400

    Added actions and values on the login form and then a controller to send an api request when the form is submitted

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,30 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     loggedIn: function() {
         return !!this.get('model');
-    }.property('model')
+    }.property('model'),
+
+    actions: {
+        login: function() {
+            var attemptedTrans = this.get('attemptedTransition');
+            var data = this.getProperties('email', 'password');
+
+            this.setProperties({
+                email: null,
+                password: null
+            });
+
+            Ember.$.post('/session', data).then(function(response) {
+                Ember.$.ajaxSetup({
+                    headers: {
+                        'Authorization': 'Bearer ' + response.api_key.access_token
+                    }
+                });
+
+            }, function(error) {
+                if (error.status === 401) {
+                    alert("Wrong email or password, please try again");
+                }
+            });
+        }
+    }
 });

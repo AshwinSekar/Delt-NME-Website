882cd6f078e44ec3ada5ca9d3ad72782e39ac8f8
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-11 22:45:46 -0400

    Added controller and route for the pledge object. Created a sample object and pushed it to the store

---
+++ app/routes/application.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function() {
+        this.store.push('pledge', {
+        	id: 0,
+            isMaster: false,
+            firstName: 'Anav',
+            lastName: 'Behl',
+            numberInterviewsDone: 2,
+            brothersInterviewed: ['David Wu', 'Ashwin Sekar']
+        });
+    },
+    setupController: function(controller) {
+        controller.set('model', this.store.find('pledge', 0));
+    }
+});

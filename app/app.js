6b30ff8e745d4978bb39438d1b7a554929e594f8
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-13 11:40:26 -0400

    Added a subclass, authenticated route, for routes that only authenticated users can reach. It checks if token has been set and if not forces them to login, remembers where they wanted to go, and then redirects them back after a successfull login

--- app/app.js
+++ app/app.js
@@ -8,13 +8,28 @@ var App;
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver,
-  LOG_TRANSITIONS: true,
-  LOG_TRANSITIONS_INTERNAL: true
+    modulePrefix: config.modulePrefix,
+    podModulePrefix: config.podModulePrefix,
+    Resolver: Resolver,
+    LOG_TRANSITIONS: true,
+    LOG_TRANSITIONS_INTERNAL: true
 });
 
 loadInitializers(App, config.modulePrefix);
+
+// Create an authenticated route for protected routes that can only be accessed after being authenticated
+App.AuthenticatedRoute = Ember.Route.extend({
+
+    beforeModel: function(transition) {
+        if (Ember.isEmpty(this.controllerFor('sessions').get('token'))) {
+            return this.redirectToLogin(transition); // If the user isn't authenticated, make them login first
+        }
+    },
+
+    redirectToLogin: function(transition) {
+        this.controllerfor('sessions').set('attemptedTransition', transition); // Store where they want to go after login
+        return this.transitionTo('sessions'); // Make them login and then transitionTo where they want to go
+    }
+});
 
 export default App;

a5ec1ea47d76f44408bfc6070c329108bc65586a
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-12 20:03:21 -0400

    Fixed ObjectController -> Controller deprecation

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
     loggedIn: function() {
         return !!this.get('model');
     }.property('model'),

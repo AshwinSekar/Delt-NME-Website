4c7cbb1eeb43c8ba3854ea0ad4516d8eef4592ad
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-12 22:31:20 -0400

    Removed the old model behavior

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,21 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {
-        this.store.push('pledge', {
-        	id: 0,
-            isMaster: false,
-            firstName: 'Anav',
-            lastName: 'Behl',
-            numberInterviewsDone: 2,
-            brothersInterviewed: ['David Wu', 'Ashwin Sekar']
-        });
-        this.store.push('pledge', {
-        	id: 1,
-        	isMaster: true
-        });
-    },
-    setupController: function(controller) {
-        controller.set('model', this.store.find('pledge', 0));
-    }
+    
 });

2e756e8395717a34dfb09794a2e94e530e169b7b
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-15 13:48:10 -0400

    Added bootstrap-material-design theme

--- front-end/Brocfile.js
+++ front-end/Brocfile.js
@@ -19,4 +19,12 @@ var app = new EmberApp();
 
 app.import("bower_components/jquery-cookie/jquery.cookie.js");
 
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+
+app.import('bower_components/bootstrap-material-design/dist/js/material.js');
+app.import('bower_components/bootstrap-material-design/dist/css/material.css');
+app.import('bower_components/bootstrap-material-design/dist/js/ripples.js');
+app.import('bower_components/bootstrap-material-design/dist/css/ripples.css');
+
 module.exports = app.toTree();

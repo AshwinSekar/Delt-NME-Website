3ef83ef7b616525452251a81606e142aca14f2d6
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-17 18:43:12 -0400

    Initial authentication attempt by looking up email password in database for a match. If so it returns the access_token associated with that pledge

--- back-end/routes/session.js
+++ back-end/routes/session.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var nedb = require('nedb');
+var datastore = new nedb({
+    filename: 'data/pledges.db'
+});
 
 router.post('/', function(res, req, next) {
-	console.log(req);
+    var email = req.req.body.email;
+    var password = req.req.body.password;
+    datastore.loadDatabase(function(err) {
+        if (err) {
+            return next({
+                status: 500,
+                message: err
+            });
+        }
+        datastore.find({email : email}, function(err,docs) {
+        	if (err) {
+        		return next({
+        			status: 500,
+        			message: err
+        		});
+        	}
+        	if (docs == null || docs.length == 0) {
+        		res.status(201).json({
+        			success: false
+        		});
+        	} else {
+        		var pledge = docs[0];
+        		res.status(201).json({
+        			success: true,
+        			api_key: {
+        				access_token: pledge.access_token,
+                        user_id: pledge.id
+        			}
+        		});
+        	}
+        });
+    });
 });
 
 module.exports = router;

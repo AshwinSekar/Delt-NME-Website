4b8222bff3a19b01ce8fce02059164ed3db59eb3
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-19 19:55:47 -0400

    Made isEditing false when opening the updatePledges route, and rollback all temporary changes so that unfinished changes do not get saved

--- front-end/app/routes/update-pledges.js
+++ front-end/app/routes/update-pledges.js
@@ -3,7 +3,14 @@ import App from '../app';
 export default App.AuthenticatedMasterRoute.extend({
 
     model: function() {
-        return this.store.find('pledge');
+        return this.store.find('pledge').then(function(pledges) {
+            pledges.forEach(function(pledge) {
+                pledge.rollback();
+                pledge.set('isEditing',false);
+                pledge.save();
+            });
+            return pledges;
+        });
     },
 
 	setupController: function(controller,model) {

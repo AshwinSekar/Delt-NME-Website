1199f37c51dfcc90941c1882b54bd88512a50c08
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-19 18:25:58 -0400

    Implemented update feature for all pledges by attaching an edit and save button to each panel

--- front-end/app/controllers/update-pledges.js
+++ front-end/app/controllers/update-pledges.js
@@ -2,15 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-	actions: {
+    actions: {
 
-		edit: function(id) {
-			console.log(id);
-		},
+        edit: function(id, isEditing) {
+            var pledge = this.get('model').filterProperty('id',id)[0];
+            if(!isEditing) {
+           		pledge.set('isEditing', true);
+           		pledge.save();
+           	} else {
+           		pledge.rollback();	
+           		pledge.set('isEditing', false);
+           		pledge.save();
+           	}
 
-		show: function(id) {
-			Ember.$('#' + id + ".hide").hide().removeClass('hide');
-			Ember.$('#' + id).toggle('slow');
-		}
-	}
+        },
+
+        save: function(id) {
+        	var pledge = this.get('model').filterProperty('id',id)[0];
+        	pledge.set('isEditing',false);
+        	pledge.save();
+        },
+        
+        show: function(id) {
+            Ember.$('#' + id + ".hide").hide().removeClass('hide');
+            Ember.$('#' + id).fadeToggle();
+        }
+    }
 });

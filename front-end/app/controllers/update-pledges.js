aa3f46ea9b8f06fc482c5390ce1a807b50c7b07b
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-19 21:50:57 -0400

    Added Delete pledge functionality

--- front-end/app/controllers/update-pledges.js
+++ front-end/app/controllers/update-pledges.js
@@ -7,60 +7,66 @@ export default Ember.Controller.extend({
     firstName: '',
     lastName: '',
     name: function() {
-      return this.get('firstName') + " " + this.get('lastName');
-    }.property('firstName','lastName'),
+        return this.get('firstName') + " " + this.get('lastName');
+    }.property('firstName', 'lastName'),
 
     actions: {
 
         edit: function(id, isEditing) {
-            var pledge = this.get('model').filterProperty('id',id)[0];
-            if(!isEditing) {
-           		pledge.set('isEditing', true);
-           		pledge.save();
-           	} else {
-           		pledge.rollback();	
-           		pledge.set('isEditing', false);
-           		pledge.save();
-           	}
+            var pledge = this.get('model').filterProperty('id', id)[0];
+            if (!isEditing) {
+                pledge.set('isEditing', true);
+                pledge.save();
+            } else {
+                pledge.rollback();
+                pledge.set('isEditing', false);
+                pledge.save();
+            }
 
         },
 
         save: function(id) {
-        	var pledge = this.get('model').filterProperty('id',id)[0];
-        	pledge.set('isEditing',false);
-        	pledge.save();
+            var pledge = this.get('model').filterProperty('id', id)[0];
+            pledge.set('isEditing', false);
+            pledge.save();
         },
 
         show: function(id) {
-        	Ember.$('#' + id + '.wow').toggleClass('dropup');
+            Ember.$('#' + id + '.wow').toggleClass('dropup');
             Ember.$('#' + id + '.hide').hide().removeClass('hide');
             Ember.$('#' + id + '.controls').fadeToggle();
         },
 
         add: function() {
-          this.setProperties({
-            adding: true,
-            email: '',
-            firstName: '',
-            lastName: '',
-          });
+            this.setProperties({
+                adding: true,
+                email: '',
+                firstName: '',
+                lastName: '',
+            });
         },
 
         saveAdd: function() {
-          var pledge = this.get('store').createRecord('pledge', {
-            isMaster: false,
-            firstName: this.get('firstName'),
-            lastName: this.get('lastName'),
-            email: this.get('email')
-          });
-          pledge.save();
-          this.set('adding',false);
+            var pledge = this.get('store').createRecord('pledge', {
+                isMaster: false,
+                firstName: this.get('firstName'),
+                lastName: this.get('lastName'),
+                email: this.get('email')
+            });
+            pledge.save();
+            this.set('adding', false);
         },
 
         closeAdd: function() {
-          this.setProperties({
-            adding: false,
-          });
+            this.setProperties({
+                adding: false,
+            });
+        },
+
+        delete: function(id) {
+            this.store.find('pledge', id).then(function(pledge) {
+                pledge.destroyRecord();
+            });
         }
     }
 });

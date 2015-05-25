7eecaa4f42e488e3332cc77afd59c7a764aa4040
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-24 20:23:29 -0400

    Added scoreboard as the homepage that shows the performance of each pledge as a progress bar

--- front-end/app/controllers/index.js
+++ front-end/app/controllers/index.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-	totalBrothers: function() {
-		return this.store.find('brothers').then(function(brothers) {
-			return brothers.get('length');
-		});
-	}
-	
 });

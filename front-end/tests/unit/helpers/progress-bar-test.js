f7e44c8774af73d1d28e63c50130c1cce8b243dd
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-24 20:22:22 -0400

    Added progress bar helper to display progress bar using handlebars

---
+++ front-end/tests/unit/helpers/progress-bar-test.js
@@ -0,0 +1,12 @@
+import {
+  progressBar
+} from '../../../helpers/progress-bar';
+import { module, test } from 'qunit';
+
+module('ProgressBarHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = progressBar(42);
+  assert.ok(result);
+});

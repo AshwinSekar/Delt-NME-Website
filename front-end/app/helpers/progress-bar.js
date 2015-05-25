8e1dd4ad17466d438393cd9baae01bbf50ef43d3
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-25 13:33:57 -0400

    Changed the progress bar helper to contextually apply styles based on completion and used striped styles. Updated the scoreboard template to fix styling issues

--- front-end/app/helpers/progress-bar.js
+++ front-end/app/helpers/progress-bar.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
 export function progressBar(value, options) {
-	var percentage = Math.floor((value[0]/value[1]) * 100) + "%";
-    return new Ember.Handlebars.SafeString('<div class="progress-bar" role="progressbar" aria-valuenow="' + value[0] +
-    									   '" aria-valuemin="0" aria-valuemax="' + value[1] + '" style="width: ' + percentage +
-    									   '"> </div>');
+    var percentage = Math.floor((value[0] / value[1]) * 100) + "%";
+    if (percentage === "100%") {
+        return new Ember.Handlebars.SafeString('<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="' + value[0] +
+            '" aria-valuemin="0" aria-valuemax="' + value[1] + '" style="min-width: 2em; width: ' + percentage +
+            '">' + value[0] + '/' + value[1] + '</div>');
+    } else {
+        return new Ember.Handlebars.SafeString('<div class="progress-bar progress-bar-striped progress-bar-info" role="progressbar" aria-valuenow="' + value[0] +
+            '" aria-valuemin="0" aria-valuemax="' + value[1] + '" style="min-width: 2em; width: ' + percentage +
+            '">' + value[0] + '/' + value[1] + '</div>');
+    }
 }
 
 export default Ember.HTMLBars.makeBoundHelper(progressBar);

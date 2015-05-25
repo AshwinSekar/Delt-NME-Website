fcc3c87313065e93e1e83825b9650937daa658b0
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-25 12:56:58 -0400

    Removed debugging info

--- front-end/app/helpers/progress-bar.js
+++ front-end/app/helpers/progress-bar.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export function progressBar(value, options) {
-	console.log("interviews done:", value[0]);
-	console.log("interviews total:", value[1]);
 	var percentage = Math.floor((value[0]/value[1]) * 100) + "%";
     return new Ember.Handlebars.SafeString('<div class="progress-bar" role="progressbar" aria-valuenow="' + value[0] +
     									   '" aria-valuemin="0" aria-valuemax="' + value[1] + '" style="width: ' + percentage +

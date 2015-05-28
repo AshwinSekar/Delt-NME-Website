2b6027dd9afd495c31c44e905103d8ab2ee9263b
Ashwin Sekar <asekar@andrew.cmu.edu>
2015-05-27 21:29:36 -0400

    Made numberInterviewsDone and Failed computed properties of their respective arrays

--- front-end/app/models/pledge.js
+++ front-end/app/models/pledge.js
@@ -4,7 +4,12 @@ export default DS.Model.extend({
     isMaster: DS.attr('boolean'),
     firstName: DS.attr('string'),
     lastName: DS.attr('string'),
-    numberInterviewsDone: DS.attr('number'),
+    numberInterviewsDone: function() {
+        return this.get('brothersInterviewed').get('length');
+    }.property('brothersInterviewed'),
+    numberInterviewsFailed: function() {
+        return this.get('brothersFailed').get('length');
+    }.property('brothersFailed'),
     brothersInterviewed: DS.hasMany('brother'),
     brothersFailed: DS.hasMany('brother'),
     isEditing: DS.attr('boolean'),
